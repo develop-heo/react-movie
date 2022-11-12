57d577154741697289176b4bd32332b058640b1a
develop-heo <44943368+develop-heo@users.noreply.github.com>
2022-11-12 11:04:25 +0900

    Movie 컴포넌트 분리

--- src/App.js
+++ src/App.js
@@ -1,35 +1,7 @@
-import { useEffect, useState} from "react";
-import Movie from "./Movie";
+import Home from './routes/Home';
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  const getMovies = async() => {
-    const response = await fetch('https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year');
-    const json = await response.json();  
-    setMovies(json.data.movies);
-    setLoading(false);
-  }
-  useEffect(() => {
-    getMovies()
-  }, []);
-  console.log(movies);
-  return (
-    <div>
-      {loading ? (<h1>Loading...</h1>) : (
-        <div>
-          {movies.map((movie) => 
-             <Movie 
-              coverImg={movie.medium_cover_image} 
-              title={movie.title}
-              summary={movie.summary}
-              genres={movie.genres}
-            />
-          )}
-        </div>
-      )}
-    </div>
-  );
+  return <Home />;
 }
 
 export default App;
